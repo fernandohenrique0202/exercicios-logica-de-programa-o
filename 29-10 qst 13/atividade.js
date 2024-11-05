8f58f4c21d16da43080bb6b2e47a0b3366d342fd
fernandohenrique0202 <fernando.costa105@etec.sp.gov.br>
2024-11-05 19:41:36 -0300

    Add files via upload

---
+++ 29-10 qst 13/atividade.js
@@ -0,0 +1,20 @@
+var altura = parseFloat(prompt("informe a altura"))
+var sexo = prompt("informe o seu genero (f = feminino/ m = masculino)")
+var peso = parseFloat(prompt("informe seu peso"))
+if (sexo = m) {
+    (72.7*altura) - 58 > peso
+    console.log("seu peso esta acima do ideal")
+    (72.7*altura) - 58 < peso 
+    console.log("seu peso esta abaixo do ideal")
+    (72.7*altura) - 58 == peso 
+    console.log("seu peso esta no ideal")
+} 
+else if (sexo = f) {
+    (62.1*altura)-44.7 > peso
+    console.log("seu peso esta acima do ideal")
+    (62.1*altura)-44.7 < peso
+    console.log("seu peso esta abaixo do ideal")
+    (62.1*altura)-44.7 == peso
+    console.log("seu peso esta no ideal")
+}
+
